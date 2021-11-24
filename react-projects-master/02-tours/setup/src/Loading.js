cf5c0fea0c66f16aefe240060444d1de91700102
Petia Valchkova <peps@gmail.com>
2021-11-24 22:01:18 +0200

    tour

---
+++ react-projects-master/02-tours/setup/src/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.gif'
+
+
+const Loading = () => {
+  return (
+    <div>
+      <img src={logo} alt="loading..." />
+      <h1>loading...</h1>
+    </div>
+  );
+};
+
+export default Loading;
