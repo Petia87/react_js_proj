0ee2d689335eafaad6098bf1ea1b86b27448470f
Petia Valchkova <peps@gmail.com>
2021-12-12 22:35:33 +0200

    hook

--- react-projects-basic/src/App.js
+++ react-projects-basic/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Final from "./tutorial/6-useReducer/final"
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+     <Final/>
     </div>
   )
 }
