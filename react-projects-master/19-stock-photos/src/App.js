34eb3fce9a377b6ddf5b7ee1434e74cec249b83e
Petia Valchkova <peps@gmail.com>
2021-12-25 23:58:37 +0200

    new

---
+++ react-projects-master/19-stock-photos/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import { FaSearch } from 'react-icons/fa'
+import Photo from './Photo'
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const mainUrl = `https://api.unsplash.com/photos/`
+const searchUrl = `https://api.unsplash.com/search/photos/`
+
+function App() {
+  const[loading, setLoaing]=useState(false);
+  const[photos, setPhotos]=useState([]);
+
+  const fetchImages = async () => {
+    setLoaing(true)
+    let url
+    url = `$(mainUrl)clientID `
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data);
+    } catch (error) {
+      setLoaing(false)
+      console.log(error);
+    }
+  }
+   useEffect(() => {
+    fetchImages() 
+     
+   }, [])
+  return <h2>stock photos starter</h2>
+}
+
+export default App
